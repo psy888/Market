03dbf2340bc7997c042e862ff4079b0953872353
psy888 <tbilisi.zp.snab@gmail.com>
2019-11-24 17:27:23 +0200

    Initial commit

---
+++ src/com/psy888/Product.java
@@ -0,0 +1,20 @@
+package com.psy888;
+
+public class Product {
+
+    private static long id = 0;
+
+    private String manufacturer;
+    private String title;
+    private String description;
+    private String[] pictureUrl;
+    private double price;
+
+    private int quantity;
+
+    public String shortInfo (){
+        String info ="";
+        return info;
+    }
+
+}
